65f741e2d0021faf6f44b00330423400a3c1ca87
rmstock <rickstockham@gmail.com>
2014-07-20 20:44:49 -0600

    Created scene.add(IRenderable)

--- src/Scene.java
+++ src/Scene.java
@@ -1,5 +1,13 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class Scene {
+	Set<IRenderable> renderables;
 	public Scene() {
-		
+		renderables = new HashSet<IRenderable>();
+	}
+	
+	public boolean add(IRenderable toAdd) {
+		return renderables.add(toAdd);
 	}
 }
