274ecaea163aacf389ba46718627d7c72f4f3d70
rmstock <rickstockham@gmail.com>
2014-08-11 21:37:54 -0700

    Turn Vector3 into a utility class, and remove Coordinate.

--- src/voxelhash/Vector3.java
+++ src/voxelhash/Vector3.java
@@ -1,44 +1,48 @@
 package voxelhash;
-import java.util.Arrays;
 
 public class Vector3{
-	private static byte dim = 3;
-	private float[] vals;
 	
-	public Vector3(float[] vals) {
-		this.vals = vals;
-	}
-	
-	public Vector3(int[] vals) {
-		this.vals = new float[3];
-		this.vals[0] = (float)vals[0];
-		this.vals[1] = (float)vals[1];
-		this.vals[2] = (float)vals[2];
-	}
-	
-	public Vector3 subtract(Vector3 second) {
-		float[] data = new float[3];
-		for (int i = 0; i < dim; i++) {
-			data[i] = vals[i] - second.vals[i];
-		}
-		return new Vector3(data);
-	}
-	
-	public Vector3 add(Vector3 second) {
-		
-		float[] data = new float[3];
-		for (int i = 0; i < dim; i++) {
-			data[i] = vals[i] + second.vals[i];
-		}
-		return new Vector3(data);
-	}
-	
-	public float getValue(int i) {
-		return vals[i];
+	private Vector3() {
 	}
 
-	public void setValue(int i, float value) {
-		vals[i] = value;
+	public static float[] makeFloats(int[] vals) {
+		return new float[] {(float) vals[0], (float) vals[1], (float) vals[2]};
+	}
+	
+	public static int[] makeInts(float[] vals) {
+		return new int[] {(int) vals[0], (int) vals[1], (int) vals[2]};
+	}
+	
+	public static float[] subtract(float[] first, float[] second) {
+		float[] data = new float[3];
+		for (int i = 0; i < 3; i++) {
+			data[i] = first[i] - second[i];
+		}
+		return data;
+	}
+	
+	public static int[] subtract(int[] first, int[] second) {
+		int[] data = new int[3];
+		for (int i = 0; i < 3; i++) {
+			data[i] = first[i] - second[i];
+		}
+		return data;
+	}
+	
+	public static float[] add(float[] first, float[] second) {
+		float[] data = new float[3];
+		for (int i = 0; i < 3; i++) {
+			data[i] = first[i] + second[i];
+		}
+		return data;
+	}
+	
+	public static int[] add(int[] first, int[] second) {
+		int[] data = new int[3];
+		for (int i = 0; i < 3; i++) {
+			data[i] = first[i] + second[i];
+		}
+		return data;
 	}
 	
 	public static void normalize(float[] vals) {
@@ -46,25 +50,5 @@ public class Vector3{
 		vals[0] /= length;
 		vals[1] /= length;
 		vals[2] /= length;
-	}
-	
-	public static Vector3 getZero() {
-		float[] zero = new float[] {0,0,0};
-		return new Vector3(zero);
-	}
-	
-	@Override
-	public int hashCode() {
-		return Arrays.hashCode(vals);
-	}
-	
-	@Override
-	public boolean equals(Object second) {
-		if (second instanceof Vector3) {
-			Vector3 two = (Vector3) second;
-			if (two.vals[0] == vals[0] && two.vals[1] == vals[1] && two.vals[2] == vals[2])
-				return true;
-		}
-		return false;
 	}
 }
